775444a08b3c288106b1c0c2ca761b60ff103357
Alena Peethala <alenadashi@icloud.com>
2023-08-08 22:41:44 -0700

    Added the first page with the three pictures to go to the various other pages. No buttons yet. Initial Commit.

--- EsteeLauder/ContentView.swift
+++ EsteeLauder/ContentView.swift
@@ -9,13 +9,84 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            HStack(spacing: 16) {
+                //  model 1 (on the left)
+                Image("Image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+      
+                
+                // EXPLORE TEXT
+                VStack {
+                    Text("EXPLORE")
+                        .font(.headline)
+                        .padding(.bottom, 8)
+                    
+                    Text("Learn more about the products in our line!")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.center)
+                  
+                        
+                }
+              
+            }
+            
+ 
+            HStack(spacing: 16) {
+                VStack {
+                    Text("FIND YOUR PERFECT MATCH")
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 8)
+                    
+                    Text("Learn more about the products in our line!")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.center)
+                }
+                
+                
+                
+                VStack {
+                    Image("Image 2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 200, height: 200)
+                       
+                    
+                }
+               
+            }
+            
+            HStack(spacing: 16) {
+                //  model 1 (on the left)
+                Image("Image 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+      
+                
+                // EXPLORE TEXT
+                VStack {
+                    Text("LEARN MORE")
+                        .font(.headline)
+                        .padding(.bottom, 8)
+                    
+                    Text("How do the ingredients in Nutritious help you get your glow on? ")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.center)
+                  
+                        
+                }
+                
+                
+            }
+            .padding(16)
         }
-        .padding()
     }
 }
 
@@ -24,3 +95,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+// need to add the buttons (to make each of these clickable and go to to the other pages) 
