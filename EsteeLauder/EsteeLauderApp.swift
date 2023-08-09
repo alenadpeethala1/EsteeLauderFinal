9e11e84993abd7d4465f6ae5ab10aa43959fd937
Alena Peethala <alenadashi@icloud.com>
2023-08-08 22:14:04 -0700

    Initial Commit

---
+++ EsteeLauder/EsteeLauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderApp.swift
+//  EsteeLauder
+//
+//  Created by Alena Peethala on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
