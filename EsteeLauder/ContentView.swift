0d3826f8fec3e65e438bab40be77c2e68bf89b8c
Alena Peethala <alenadashi@icloud.com>
2023-08-08 23:31:07 -0700

    Hiii Im hoping this commit works?

--- EsteeLauder/ContentView.swift
+++ EsteeLauder/ContentView.swift
@@ -9,84 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 16) {
-            HStack(spacing: 16) {
-                //  model 1 (on the left)
-                Image("Image")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200, height: 200)
-      
-                
-                // EXPLORE TEXT
-                VStack {
-                    Text("EXPLORE")
-                        .font(.headline)
-                        .padding(.bottom, 8)
-                    
-                    Text("Learn more about the products in our line!")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                        .multilineTextAlignment(.center)
-                  
-                        
-                }
-              
-            }
-            
- 
-            HStack(spacing: 16) {
-                VStack {
-                    Text("FIND YOUR PERFECT MATCH")
-                        .font(.headline)
-                        .multilineTextAlignment(.center)
-                        .padding(.bottom, 8)
-                    
-                    Text("Learn more about the products in our line!")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                        .multilineTextAlignment(.center)
-                }
-                
-                
-                
-                VStack {
-                    Image("Image 2")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 200, height: 200)
-                       
-                    
-                }
-               
-            }
-            
-            HStack(spacing: 16) {
-                //  model 1 (on the left)
-                Image("Image 1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 200, height: 200)
-      
-                
-                // EXPLORE TEXT
-                VStack {
-                    Text("LEARN MORE")
-                        .font(.headline)
-                        .padding(.bottom, 8)
-                    
-                    Text("How do the ingredients in Nutritious help you get your glow on? ")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                        .multilineTextAlignment(.center)
-                  
-                        
-                }
-                
-                
-            }
-            .padding(16)
-        }
+        Text("Hello,World!")
+        
     }
 }
 
@@ -95,6 +19,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-// need to add the buttons (to make each of these clickable and go to to the other pages) 
---
+++ EsteeLauder/Home.swift
@@ -0,0 +1,89 @@
+//
+//  Home.swift
+//  EsteeLauder
+//
+//  Created by Alena Peethala on 8/8/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(destination: Explore()) {
+                    HStack {
+                        Image("Image")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 200, height: 220)
+                            .clipped()
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("LEARN")
+                                .font(.title)
+                                .multilineTextAlignment(.center)
+                            
+                            Text("Browse our line and discover how the ingredients in Nutritious can help you glow!")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                }
+                
+                NavigationLink(destination: SkincareQuiz()) {
+                    HStack {
+                        VStack {
+                            Text("FIND YOUR MATCH")
+                                .font(.title)
+                                .multilineTextAlignment(.trailing)
+                            
+                            Text("Learn more about the products in our line!")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+                        Image("Image 2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 200, height: 220)
+                            .clipped()
+                    }
+                }
+                
+                NavigationLink(destination: Explore()) {
+                    HStack {
+                        // Big square image on the left
+                        Image("Image 1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .padding(.trailing)
+                            .frame(width: 220, height: 220)
+                            .clipped()
+                        
+                        // Square of text on the right of the explore image..
+                        VStack(alignment: .leading, spacing: 16) {
+                            Text("EXPLORE")
+                                .font(.title)
+                                .multilineTextAlignment(.leading)
+                            
+                            Text("Browse our line of products!")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                }
+                .padding(16)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
